409631e40a827c93cb6a447ad21cbd185acd1a5e
宋冬生 <dongsheng.song@gmail.com>
2014-11-05 23:55:21 +0800

    Make openssl/s_socket.c compileable on Windows.

--- src/usr.bin/openssl/s_socket.c
+++ src/usr.bin/openssl/s_socket.c
@@ -56,12 +56,15 @@
  * [including the GNU Public Licence.]
  */
 
+#ifdef _WIN32
+#include <ws2tcpip.h>
+#else
 #include <sys/socket.h>
-
 #include <netinet/in.h>
+#include <netdb.h>
+#endif
 
 #include <errno.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
